10c0cd61a3d0765341eaad3f2ee94d13f87fee89
Ajay Solanki <ajaysolanki.pvt@gmail.com>
2025-01-29 13:15:41 +0530

    add content in one.py

---
+++ one.py
@@ -0,0 +1,7 @@
+# ### **14. Monitoring with PM2**
+
+# - Introduction to PM2 for Node.js process management
+# - Setting up PM2 for application monitoring
+# - Auto-restarting Node.js apps on crashes
+# - Monitoring performance with PM2 logs and stats
+# - Log rotations
